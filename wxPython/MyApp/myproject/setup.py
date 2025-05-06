27e4810462d1d8e8710be68edbe7bbb92a3c5284
mureee-18 <kflwwa@gmail.com>
2025-05-06 21:31:51 +0900

    commit5

--- wxPython/MyApp/myproject/setup.py
+++
@@ -1,9 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(
-    name='MyApp',
-    version='1.1',
-    description='Sample Application',
-    options={'build_exe': {'includes': ['pandas']}},
-    executables=[Executable('__main__.py')]
-)
